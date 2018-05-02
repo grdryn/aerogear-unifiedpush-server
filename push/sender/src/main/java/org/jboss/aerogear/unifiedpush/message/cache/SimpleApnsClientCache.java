5b4fd190050aa78e4a6a5a0d8cf28d5542a29724
Matthias Wessendorf <matzew@apache.org>
2018-05-02 13:47:29 +0200

    AEROGEAR-2659 - check if cert is valid, otherwise do not try to connect (#999)
    * :no_entry_sign: check if cert is valid, otherwise do not try to connect
    
    * :accept: fix npe and cleanup changes
    
    * :passport_control: move cert check to start of sender method
    
    * :no_entry: Adding some sort of CN check

--- push/sender/src/main/java/org/jboss/aerogear/unifiedpush/message/cache/SimpleApnsClientCache.java
+++ push/sender/src/main/java/org/jboss/aerogear/unifiedpush/message/cache/SimpleApnsClientCache.java
@@ -82,7 +82,7 @@ public class SimpleApnsClientCache {
             synchronized (apnsClientExpiringMap) {
                 client = constructor.construct();
 
-                if (client.isConnected()) {
+                if (client != null && client.isConnected()) {
                     putApnsClientForVariantID(connectionKey, client);
                 }
 
